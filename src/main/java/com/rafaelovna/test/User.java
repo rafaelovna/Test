56785bb44d19b6a8e27c4b3165ec5fd4452c8e76
rafaelovna <aikanush777@mail.ru>
2023-03-05 22:18:08 +0400

    ДЗ

--- src/main/java/com/rafaelovna/test/User.java
+++ src/main/java/com/rafaelovna/test/User.java
@@ -1,6 +1,7 @@
 package com.rafaelovna.test;
 
 import lombok.ToString;
+
 import java.util.Objects;
 
 @ToString
@@ -11,42 +12,33 @@ public class User {
     private String email;
 
 
-
     public User(String login, String email) {
         if (login == null || login.isEmpty()) {
-            System.out.println("Введите логин или почту");
+            throw new RuntimeException("Введите логин или почту");
         } else if (login.contains("@") || login.contains(".")) {
-            System.out.println("Логин не может состоять из символов '@' и '.'");
+            throw new RuntimeException("Логин не может состоять из символов '@' и '.'");
         } else {
             this.login = login;
         }
 
         if (email == null || email.isEmpty()) {
-            System.out.println("Введите логин или почту");
+            throw new RuntimeException("Введите логин или почту");
         } else if (!email.contains("@") || !email.contains(".")) {
-            System.out.println("Электронная почта должна включать символы: '@' и '.'");
+            throw new RuntimeException("Электронная почта должна включать символы: '@' и '.'");
         } else {
             this.email = email;
         }
     }
-    public User() {
+    public User() {}
 
-    }
     public String getLogin() {
-            return login;
+        return login;
     }
 
     public String getEmail() {
-            return email;
+        return email;
     }
 
-    public boolean correctEmail(String email) {
-        return (email.contains(".") && email.contains("@"));
-    }
-
-    public boolean equalLoginAndEmail(String login, String email) {
-        return !Objects.equals(login, email);
-    }
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
--- src/test/java/com/rafaelovna/test/UserTest.java
+++ src/test/java/com/rafaelovna/test/UserTest.java
@@ -3,11 +3,14 @@ package com.rafaelovna.test;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+
 public class UserTest {
 
 
     public static final String INCORRECT_EMAIL = "annamailru";
+    public static final String INCORRECT_LOGIN = "anna.@";
     public static final String CORRECT_EMAIL = "anna@mail.ru";
+    public static final String CORRECT_LOGIN = "anna";
 
 
     private final User out = new User("anna", "anna@mail.ru");
@@ -22,17 +25,21 @@ public class UserTest {
     @Test
     public void checkingTheDataInstallationWhenCreatingAnObject() {
         User result = new User();
-        Assertions.assertNotNull(result);
+        Assertions.assertNull(result.getLogin());
+        Assertions.assertNull(result.getEmail());
     }
 
     @Test
     public void emailAddressCorrectnessTest() {
-        Assertions.assertFalse(out.correctEmail("anna@mailru"), INCORRECT_EMAIL);
-        Assertions.assertTrue(out.correctEmail("anna@mail.ru"), CORRECT_EMAIL);
+        Assertions.assertThrows(RuntimeException.class, () -> new User(CORRECT_LOGIN, INCORRECT_EMAIL));
+    }
+    @Test
+    public void loginCorrectnessTest() {
+        Assertions.assertThrows(RuntimeException.class, () -> new User(INCORRECT_LOGIN, CORRECT_EMAIL));
     }
 
     @Test
-    public void checkingForTheEqualityOfLoginAndEmail() {
-        Assertions.assertFalse(out.equalLoginAndEmail("anna", "anna"));
+    public void loginAndEmailAreEqual() {
+        Assertions.assertThrows(RuntimeException.class, () -> new User("anna","anna"));
     }
 }
